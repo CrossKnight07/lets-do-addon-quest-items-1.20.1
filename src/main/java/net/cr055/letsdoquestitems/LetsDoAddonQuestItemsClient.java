5b39a81cca145717be1ceab94ebf03b4184412ff
CrossKnight07 <ileshwetzels@ziggo.nl>
2025-02-09 19:39:28 +0100

    Initial commit

---
+++ src/main/java/net/cr055/letsdoquestitems/LetsDoAddonQuestItemsClient.java
@@ -0,0 +1,10 @@
+package net.cr055.letsdoquestitems;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LetsDoAddonQuestItemsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/cr055/letsdoquestitems/LetsDoAddonQuestItemsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.cr055.letsdoquestitems;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LetsDoAddonQuestItemsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
